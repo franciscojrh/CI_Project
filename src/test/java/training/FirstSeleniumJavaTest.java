ad367f7c4cf476ba3047633090cea3bbed701e23
Francisco RH <francisco.rh@wizeline.com>
2022-04-01 11:57:59 -0600

    First commit

---
+++ src/test/java/training/FirstSeleniumJavaTest.java
@@ -0,0 +1,35 @@
+package training;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstSeleniumJavaTest {
+    private WebDriver driver;
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30000, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testEasy(){
+        driver.get("https://testautomationu.applitools.com/");
+        String title = driver.getTitle();
+        AssertJUnit.assertTrue(title.equals("Test Automation University | Applitools"));
+    }
+
+    @AfterTest
+    public void tearDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
